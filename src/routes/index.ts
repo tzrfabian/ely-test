f342caf588b0508b661a1b14d8cd513b817e1072
Fabian Tenzara <fabiantenzara@gmail.com>
2025-03-04 09:26:28 +0700

    refac: adjustment in routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,8 +4,10 @@ import { addUser } from '../controllers/UserController'
 
 const userRoutes = new Elysia({ prefix: '/users' })
     .get('/', () => getAllUsers())
-    .post('/', (req: {body: {
-        email: string, username: string, password: string
-    }}) => addUser(req.body.email, req.body.username, req.body.password))
+    .post('/', (req: {
+        body: {
+            email: string, username: string, password: string
+        }
+    }) => addUser(req.body.email, req.body.username, req.body.password))
 
 export default userRoutes
